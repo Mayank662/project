87a7f95f4559232ba517000f8c82190ef30e20e5
mayank662 <mayankgoyal662@gmail.com>
2022-03-08 19:03:46 +0530

    done with all task

--- zssn/survivor_info/urls.py
+++ zssn/survivor_info/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('survivors', views.survivor_fun, name = 'survivor_insert'),
-    path('<int:id>/',views.survivor_fun, name = 'survivor_update'),
-#    path('list/',views.survivor_list, name = 'survivor_list')
+    path('survivors/<int:id>/',views.survivor_fun, name = 'survivor_update'),
+    path('survivors/reports/',views.report, name = 'survivor_report')
 ]
--- zssn/survivor_info/views.py
+++ zssn/survivor_info/views.py
@@ -31,3 +31,16 @@ def survivor_fun(request, id=0):
         context = {'survivor_list':survivorModel.objects.all()}
         return render(request,'survivor_list.html',context)
         
+def report(request):
+    infect_count = 0
+    non_infect_count = 0
+    survivor_data = list(survivorModel.objects.all())
+    total_person = len(survivor_data)
+    for i in range(total_person):
+        if(survivor_data[i].infected):
+            infect_count = infect_count+1
+        else:
+            non_infect_count = non_infect_count+1
+    infect_per = (infect_count/total_person)*100
+    non_infect_per = (non_infect_count/total_person)*100   
+    return render(request,'result.html',{'infect_per':infect_per,'non_infect_per':non_infect_per})
